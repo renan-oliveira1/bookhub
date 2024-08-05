81b5c37f47506409c2c55d4756d16f6f1664ed36
Renan Oliveira <renan.oliveira.rao@gmail.com>
2024-08-05 18:07:05 -0300

    Add service in kotlin for the reviews of the book

---
+++ review/src/test/kotlin/com/bookhub/review/ReviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bookhub.review
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
