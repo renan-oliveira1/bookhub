9b2d63d6dc52305fe20b952b6de6ea3529cd001e
Renan Oliveira <renan.oliveira.rao@gmail.com>
2024-08-12 11:27:47 -0300

    Implement config server to users service

--- users/src/main/java/com/bookhub/users/UsersApplication.java
+++ users/src/main/java/com/bookhub/users/UsersApplication.java
@@ -1,9 +1,12 @@
 package com.bookhub.users;
 
+import com.bookhub.users.dto.UsersContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {UsersContactInfoDto.class})
 public class UsersApplication {
 
 	public static void main(String[] args) {
--- users/src/main/java/com/bookhub/users/controllers/UserController.java
+++ users/src/main/java/com/bookhub/users/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.bookhub.users.controllers;
 
 import com.bookhub.users.dto.UserDto;
+import com.bookhub.users.dto.UsersContactInfoDto;
 import com.bookhub.users.dto.response.UserResponseDto;
 import com.bookhub.users.entities.User;
 import com.bookhub.users.services.IUserService;
 import com.bookhub.users.services.UserService;
 import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,8 @@ import java.util.List;
 public class UserController {
 
     private IUserService userService;
+    @Autowired
+    private UsersContactInfoDto usersContactInfoDto;
 
     public UserController(IUserService userService) {
         this.userService = userService;
@@ -49,4 +53,8 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body("User Deleted!");
     }
 
+    @GetMapping("/contact-info")
+    public ResponseEntity<UsersContactInfoDto> getContactInfo(){
+        return ResponseEntity.status(HttpStatus.OK).body(usersContactInfoDto);
+    }
 }
